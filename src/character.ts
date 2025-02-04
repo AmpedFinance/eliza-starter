c8221b7e9deb7642bf82e1aae237e430a11de632
dancube <denright@gmail.com>
2025-02-04 14:41:34 +1100

    clients

--- src/character.ts
+++ src/character.ts
@@ -4,7 +4,7 @@ export const character: Character = {
     ...defaultCharacter,
     "name": "Eliza",
     "plugins": [],
-    "clients": [],
+    "clients": ["twitter", "discord"],
     "modelProvider": "openrouter",
     "settings": {
       "secrets": {},
